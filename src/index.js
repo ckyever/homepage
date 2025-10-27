ec35674af716a674c7896eea064e5ae0f1c717f2
Ckyever Gaviola <ckyeverg@gmail.com>
2025-10-27 19:40:17 +1100

    Attempt to serve the resume file via webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./styles.css";
 import { appendProjects } from "./projectList";
+import resumeFile from "../public/resume-ckyever-gaviola.pdf";
 
 const projectList = document.querySelector("#project-list");
 appendProjects(projectList);
+
+const resumeLink = document.querySelector("#resume-link");
+resumeLink.href = resumeFile;
