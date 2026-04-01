d6f8f59ae0aac65557515aae52ea898ce06e7180
Ckyever Gaviola <ckyeverg@gmail.com>
2026-04-01 23:10:21 +1100

    Completely remove resume asset, icon, and links

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import "./styles.css";
 import { appendProjects } from "./projectList";
-import resumeFile from "../public/resume-ckyever-gaviola.pdf";
 
 const projectList = document.querySelector("#project-list");
 appendProjects(projectList);
 
-const resumeLinks = document.querySelectorAll(".resume-link");
-resumeLinks.forEach((link) => (link.href = resumeFile));
